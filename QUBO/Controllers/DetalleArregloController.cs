19a5fc3fc54d5efaf1ace33b8121269a521cd708
Julian E. Gomez <julianegomez12@hotmail.com>
2024-06-17 19:16:53 -0300

    Formulario de arreglo v1

---
+++ QUBO/Controllers/DetalleArregloController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QUBO.Models;
+
+namespace QUBO.Controllers
+{
+    public class DetalleArregloController : Controller
+    {
+        private readonly QuboDbContext _context;
+
+        public DetalleArregloController(QuboDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DetalleArreglo
+        public async Task<IActionResult> Index()
+        {
+            var quboDbContext = _context.DetalleArreglos.Include(d => d.IdArregloNavigation).Include(d => d.IdParteNavigation);
+            return View(await quboDbContext.ToListAsync());
+        }
+
+        // GET: DetalleArreglo/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var detalleArreglo = await _context.DetalleArreglos
+                .Include(d => d.IdArregloNavigation)
+                .Include(d => d.IdParteNavigation)
+                .FirstOrDefaultAsync(m => m.IdDetalle == id);
+            if (detalleArreglo == null)
+            {
+                return NotFound();
+            }
+
+            return View(detalleArreglo);
+        }
+
+        // GET: DetalleArreglo/Create
+        public IActionResult Create()
+        {
+            ViewData["IdArreglo"] = new SelectList(_context.Arreglos, "IdArreglo", "IdArreglo");
+            ViewData["IdParte"] = new SelectList(_context.Partes, "IdPartes", "IdPartes");
+            return View();
+        }
+
+        // POST: DetalleArreglo/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdDetalle,IdArreglo,IdParte,Descripcion")] DetalleArreglo detalleArreglo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(detalleArreglo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdArreglo"] = new SelectList(_context.Arreglos, "IdArreglo", "IdArreglo", detalleArreglo.IdArreglo);
+            ViewData["IdParte"] = new SelectList(_context.Partes, "IdPartes", "IdPartes", detalleArreglo.IdParte);
+            return View(detalleArreglo);
+        }
+
+        // GET: DetalleArreglo/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var detalleArreglo = await _context.DetalleArreglos.FindAsync(id);
+            if (detalleArreglo == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdArreglo"] = new SelectList(_context.Arreglos, "IdArreglo", "IdArreglo", detalleArreglo.IdArreglo);
+            ViewData["IdParte"] = new SelectList(_context.Partes, "IdPartes", "IdPartes", detalleArreglo.IdParte);
+            return View(detalleArreglo);
+        }
+
+        // POST: DetalleArreglo/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("IdDetalle,IdArreglo,IdParte,Descripcion")] DetalleArreglo detalleArreglo)
+        {
+            if (id != detalleArreglo.IdDetalle)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(detalleArreglo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DetalleArregloExists(detalleArreglo.IdDetalle))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdArreglo"] = new SelectList(_context.Arreglos, "IdArreglo", "IdArreglo", detalleArreglo.IdArreglo);
+            ViewData["IdParte"] = new SelectList(_context.Partes, "IdPartes", "IdPartes", detalleArreglo.IdParte);
+            return View(detalleArreglo);
+        }
+
+        // GET: DetalleArreglo/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var detalleArreglo = await _context.DetalleArreglos
+                .Include(d => d.IdArregloNavigation)
+                .Include(d => d.IdParteNavigation)
+                .FirstOrDefaultAsync(m => m.IdDetalle == id);
+            if (detalleArreglo == null)
+            {
+                return NotFound();
+            }
+
+            return View(detalleArreglo);
+        }
+
+        // POST: DetalleArreglo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var detalleArreglo = await _context.DetalleArreglos.FindAsync(id);
+            if (detalleArreglo != null)
+            {
+                _context.DetalleArreglos.Remove(detalleArreglo);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DetalleArregloExists(long id)
+        {
+            return _context.DetalleArreglos.Any(e => e.IdDetalle == id);
+        }
+    }
+}
---
+++ QUBO/Controllers/ParteController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QUBO.Models;
+
+namespace QUBO.Controllers
+{
+    public class ParteController : Controller
+    {
+        private readonly QuboDbContext _context;
+
+        public ParteController(QuboDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Parte
+        public async Task<IActionResult> Index()
+        {
+            var quboDbContext = _context.Partes.Include(p => p.IdCelularNavigation);
+            return View(await quboDbContext.ToListAsync());
+        }
+
+        // GET: Parte/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parte = await _context.Partes
+                .Include(p => p.IdCelularNavigation)
+                .FirstOrDefaultAsync(m => m.IdPartes == id);
+            if (parte == null)
+            {
+                return NotFound();
+            }
+
+            return View(parte);
+        }
+
+        // GET: Parte/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCelular"] = new SelectList(_context.Celulars, "IdCelular", "IdCelular");
+            return View();
+        }
+
+        // POST: Parte/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPartes,Nombre,Modelo,PrecioUsd,CodigoProducto,IdCelular")] Parte parte)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(parte);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCelular"] = new SelectList(_context.Celulars, "IdCelular", "IdCelular", parte.IdCelular);
+            return View(parte);
+        }
+
+        // GET: Parte/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parte = await _context.Partes.FindAsync(id);
+            if (parte == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCelular"] = new SelectList(_context.Celulars, "IdCelular", "IdCelular", parte.IdCelular);
+            return View(parte);
+        }
+
+        // POST: Parte/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("IdPartes,Nombre,Modelo,PrecioUsd,CodigoProducto,IdCelular")] Parte parte)
+        {
+            if (id != parte.IdPartes)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(parte);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ParteExists(parte.IdPartes))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCelular"] = new SelectList(_context.Celulars, "IdCelular", "IdCelular", parte.IdCelular);
+            return View(parte);
+        }
+
+        // GET: Parte/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parte = await _context.Partes
+                .Include(p => p.IdCelularNavigation)
+                .FirstOrDefaultAsync(m => m.IdPartes == id);
+            if (parte == null)
+            {
+                return NotFound();
+            }
+
+            return View(parte);
+        }
+
+        // POST: Parte/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var parte = await _context.Partes.FindAsync(id);
+            if (parte != null)
+            {
+                _context.Partes.Remove(parte);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ParteExists(long id)
+        {
+            return _context.Partes.Any(e => e.IdPartes == id);
+        }
+    }
+}
