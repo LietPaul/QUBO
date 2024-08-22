8dd81c8f13f18707fc9714c40918538e459d588b
LietPaul <93621221+LietPaul@users.noreply.github.com>
2024-08-21 21:47:29 -0300

    Modelo paginador

---
+++ QUBO/Models/Pager.cs
@@ -0,0 +1,19 @@
+﻿namespace QUBO.Models
+{
+    public class Pager
+    {
+        public int TotalItems { get; private set; }
+        public int CurrentPage { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalPages { get; private set; }
+        public int StartPage { get; private set; }
+        public int EndPage { get; private set; }
+
+        public Pager() { }
+        public Pager(int totalitems, int page, int pagesize = 10)
+        {
+            int totalPages = (int)Math.Ceiling((decimal)totalitems / (decimal)pagesize);
+            int currentPage = page;
+        }
+    }
+}
